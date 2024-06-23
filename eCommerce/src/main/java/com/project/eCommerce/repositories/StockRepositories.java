b047fd74231cf7e971a76f74f6fbb33f5aa0e3b4
José Junior <gilson.pjd@gmail.com>
2024-06-23 14:24:38 +0100

    Deleted the unnecessary librarys.

--- eCommerce/src/main/java/com/project/eCommerce/repositories/StockRepositories.java
+++ eCommerce/src/main/java/com/project/eCommerce/repositories/StockRepositories.java
@@ -3,7 +3,6 @@ package com.project.eCommerce.repositories;
 import com.project.eCommerce.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
 import java.util.UUID;
 
 public interface StockRepositories extends JpaRepository<Product, UUID> {
